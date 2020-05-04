9bb3e45f25623227c8508fcdd6e980723ed9ae61
Lockdain <lockdain@gmail.com>
2020-05-04 21:59:34 +0300

    Reduce operator was added.

--- src/main/java/com/alexsergeenko/flink/reduce/AverageProfit.java
+++ src/main/java/com/alexsergeenko/flink/reduce/AverageProfit.java
@@ -1,5 +1,9 @@
 package com.alexsergeenko.flink.reduce;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -11,8 +15,41 @@ public class AverageProfit {
 
         DataStream<String> data = env.readTextFile("/Users/user/IdeaProjects/Flink/flinktutorial/src/main/resources/reduce/avg");
 
+        DataStream<Tuple5<String, String, String, Integer, Integer>> mapped = data.map(new Splitter());
 
+        DataStream<Tuple5<String, String, String, Integer, Integer>> reduced = mapped.keyBy(0).reduce(new Reduce1());
+        //reduced.print();
 
-        env.execute("ReduceExample");
+        // Month avg profit
+        DataStream<Tuple2<String, Double>> profitPerMonth = reduced.map(new MapFunction<Tuple5<String, String, String, Integer, Integer>, Tuple2<String, Double>>() {
+            @Override
+            public Tuple2<String, Double> map(Tuple5<String, String, String, Integer, Integer> input) throws Exception {
+                return new Tuple2<String, Double>(input.f0, new Double(input.f3 * 1.0) / input.f4);
+            }
+        });
+
+        profitPerMonth.print();
+
+        env.execute("Reduce Example");
+
+    }
+
+    public static class Splitter implements MapFunction<String, Tuple5<String, String, String, Integer, Integer>> {
+
+        @Override
+        public Tuple5<String, String, String, Integer, Integer> map(String s) throws Exception {
+            String[] strings = s.split(",");
+            return new Tuple5<String, String, String, Integer, Integer>(strings[1], strings[2], strings[3], Integer.parseInt(strings[4]), 1);
+        }
+    }
+
+    public static class Reduce1 implements ReduceFunction<Tuple5<String, String, String, Integer, Integer>> {
+
+        @Override
+        public Tuple5<String, String, String, Integer, Integer> reduce(Tuple5<String, String, String, Integer, Integer> current,
+                                                                       Tuple5<String, String, String, Integer, Integer> pre_result) {
+            return new Tuple5<>(current.f0, current.f1, current.f2, current.f3 + pre_result.f3, current.f4 + pre_result.f4);
+        }
     }
 }
+
