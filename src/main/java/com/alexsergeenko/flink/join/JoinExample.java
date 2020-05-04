131e06d2892300a13304d90b8d7d55c92b03ab09
Lockdain <lockdain@gmail.com>
2020-05-04 17:23:03 +0300

    Join Function has been implemented.

---
+++ src/main/java/com/alexsergeenko/flink/join/JoinExample.java
@@ -0,0 +1,58 @@
+package com.alexsergeenko.flink.join;
+
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.MapOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+public class JoinExample {
+
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        env.getConfig().setGlobalJobParameters(params);
+
+        DataSet<String> personSet = env.readTextFile(params.get("input1"));
+        DataSet<String> locationSet = env.readTextFile(params.get("input2"));
+
+        // Read a file of "1, Mike 2, Joe" and map it to tuples
+        MapOperator<String, Tuple2<Integer, String>> persons = personSet
+                .map(new MapFunction<String, Tuple2<Integer, String>>() {
+                    @Override
+                    public Tuple2<Integer, String> map(String value) {
+                        String[] words = value.split(",");
+                        return new Tuple2<>(Integer.parseInt(words[0]), words[1]);
+                    }});
+
+        // Read a locations file
+        DataSet<Tuple2<Integer, String>> locations = locationSet
+                .map(new MapFunction<String, Tuple2<Integer, String>>() {
+                    @Override
+                    public Tuple2<Integer, String> map(String value) throws Exception {
+                        String[] words = value.split(",");
+                        return new Tuple2<>(Integer.parseInt(words[0]), words[1]);
+                    }
+                });
+
+        // Join datasets on personId
+        // The result will be <id, personName, state>
+        DataSet<Tuple3<Integer, String, String>> joined =
+                persons
+                        .join(locations)
+                        .where(0)
+                        .equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
+                    @Override
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person, Tuple2<Integer, String> location) {
+                        return new Tuple3<>(person.f0, person.f1, location.f1);
+                    }
+                });
+
+        joined.writeAsCsv(params.get("output"), "\n", " ");
+        env.execute("Join Example");
+    }
+}
